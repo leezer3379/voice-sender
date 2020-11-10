cfebce99ee72d9dbabc17b430ff6ef3b32868a62
lize <lize@shouqiev.com>
2020-11-10 16:09:49 +0800

    update

--- main.go
+++ main.go
@@ -78,7 +78,8 @@ func sendVoice(w http.ResponseWriter, r *http.Request) {
 
 	r.ParseForm()  //解析参数，默认是不会解析的
 	fmt.Println(r.Form)  //这些信息是输出到服务器端的打印信息
-	fmt.Println(r.GetBody)
+	fmt.Println(r.PostForm)
+	fmt.Println(r.Body)
 	fmt.Println("path", r.URL.Path)
 	fmt.Println("scheme", r.URL.Scheme)
 	fmt.Println(r.Form["url_long"])
