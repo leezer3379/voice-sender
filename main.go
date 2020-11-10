371abc9ac13aa2bd7f839427a53e657b137244bd
lize <lize@shouqiev.com>
2020-11-10 16:17:55 +0800

    update

--- main.go
+++ main.go
@@ -76,18 +76,15 @@ func startHttp() {
 
 func sendVoice(w http.ResponseWriter, r *http.Request) {
 
-	r.ParseForm()  //解析参数，默认是不会解析的
-	fmt.Println(r.Form)  //这些信息是输出到服务器端的打印信息
-	fmt.Println(r.PostForm)
-	fmt.Println(r.Body)
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Println(r.Form["url_long"])
-	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
+	fmt.Println("method:", r.Method) //获取请求的方法
+	if r.Method == "GET" {
+
+		fmt.Println("OK")
+	} else {
+		//请求的是登录数据，那么执行登录的逻辑判断
+		fmt.Println("username:", r.Form["username"])
+		fmt.Println("password:", r.Form["password"])
 	}
-	fmt.Fprintf(w, "Hello astaxie!") //这个写入到w的是输出到客户端的
 }
 
 func ending() {
