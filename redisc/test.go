d2899f588698f707a8351bde964f401aa0b5e007
lize <lize@shouqiev.com>
2021-08-26 22:10:12 +0800

    update

--- redisc/test.go
+++ redisc/test.go
@@ -1,8 +1,8 @@
-package main
+package redisc
 
 import "github.com/leezer3379/voice-sender/cron"
 
-func main()  {
+func test()  {
 	cron.V3SendDingTalk("daad3cccd4b6af8733147818c397597dfef13c6149570bb41c5493a9019c07ff", "备份0test1", []string{"18810223379"})
 
 }
